0824a8c8d44b2773f52829c0d3bb620caa417a97
pagr0m <pagrom27@gmail.com>
2021-03-25 22:33:26 +0100

    Check test on macOS

--- plugin-build/plugin/src/test/java/org/zoldater/kotlin/gradle/spm/plugin/FunctionalTest.kt
+++ plugin-build/plugin/src/test/java/org/zoldater/kotlin/gradle/spm/plugin/FunctionalTest.kt
@@ -39,9 +39,32 @@ class FunctionalTest {
             .withPluginClasspath()
             .build()
 
-        assertTrue(result.output.contains("Hello world!"))
+        assertTrue(result.output.contains(initializeTaskOutput(Family.IOS), true))
         assertEquals(TaskOutcome.SUCCESS, result.task(":$taskName")?.outcome)
 
-        println("Tasks: ${result.tasks}")
+        val sameResult = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments(taskName)
+            .withPluginClasspath()
+            .build()
+
+        assertTrue(result.output.contains(initializeTaskOutput(Family.IOS), true))
+        assertEquals(TaskOutcome.UP_TO_DATE, sameResult.task(":$taskName")?.outcome)
+    }
+
+    private fun initializeTaskOutput(family: Family): String {
+        return """
+        Creating library package: ${family.name}
+        Creating Package.swift
+        Creating README.md
+        Creating .gitignore
+        Creating Sources/
+        Creating Sources/${family.name}/${family.name}.swift
+        Creating Tests/
+        Creating Tests/LinuxMain.swift
+        Creating Tests/${family.name}Tests/
+        Creating Tests/${family.name}Tests/${family.name}Tests.swift
+        Creating Tests/${family.name}Tests/XCTestManifests.swift
+    """.trimIndent()
     }
 }

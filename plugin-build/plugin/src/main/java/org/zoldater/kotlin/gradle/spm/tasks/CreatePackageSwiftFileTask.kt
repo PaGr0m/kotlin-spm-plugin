851eb383a1ba023f4d2d1fa40343e5d2c13ee7be
Yaroslav Chernyshev <yaroslav.chernyshev@jetbrains.com>
2021-03-31 19:06:25 +0300

    Fix up-to-date checks for CreatePackageSwiftFileTask

--- plugin-build/plugin/src/main/java/org/zoldater/kotlin/gradle/spm/tasks/CreatePackageSwiftFileTask.kt
+++ plugin-build/plugin/src/main/java/org/zoldater/kotlin/gradle/spm/tasks/CreatePackageSwiftFileTask.kt
@@ -4,10 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.Nested
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import org.jetbrains.kotlin.konan.target.Family
 import org.zoldater.kotlin.gradle.spm.entity.impl.DependencyManager
 import org.zoldater.kotlin.gradle.spm.plugin.KotlinSpmPlugin
@@ -25,7 +22,7 @@ abstract class CreatePackageSwiftFileTask : DefaultTask() {
     @Nested
     val platformFamily: Property<Family> = project.objects.property(Family::class.java)
 
-    @Nested
+    @Input
     val platformVersion: Property<String> = project.objects.property(String::class.java)
 
     @Nested
@@ -35,12 +32,6 @@ abstract class CreatePackageSwiftFileTask : DefaultTask() {
     @get:OutputFile
     val outputPlatformPackageSwiftFile: Provider<File>
         get() = platformFamily.map { project.swiftPackageBuildDirs.packageSwiftFile(it) }
-
-    @get:OutputDirectory
-    val outputXcodeBuildDirectory: Provider<File>
-        get() = platformFamily.map {
-            project.swiftPackageBuildDirs.platformRoot(it).resolve(".build")
-        }
 
     @TaskAction
     fun action() {
